81bbc694884874859af442df743caef9a26a94f1
Roshini Jaiswal <roshini.s.jaiswal@gmail.com>
2019-01-18 00:46:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(feet) {
   } else if (feet > 2500) {
     return "No can do"
   } else if (feet > 2000) {
-    return ""
+    return "'I will gladly take your thirty bucks."
   }
 }
 
