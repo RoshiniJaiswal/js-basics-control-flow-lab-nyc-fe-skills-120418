a89c3e59c1f06db801747d729983f94537c2b201
Roshini Jaiswal <roshini.s.jaiswal@gmail.com>
2019-01-18 01:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case "generous" : {
-      return "Thankyou so much."
+      return "Thank you so much."
     }
   }
 }
