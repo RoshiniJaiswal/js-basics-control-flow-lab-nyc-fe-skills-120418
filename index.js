de47943b88ce18ab56257f0ffe1cd442aa1e24b9
Roshini Jaiswal <roshini.s.jaiswal@gmail.com>
2019-01-18 01:05:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ function ternaryCheckCity(city) {
  return (city==="NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    
+  }
+}
    
    
