9a424051058fffdf982c8f1d11a04a5982ae6079
Roshini Jaiswal <roshini.s.jaiswal@gmail.com>
2019-01-18 00:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
-  if (feet <= 400) {
+  if (feet <= 400){
     return "This is on me!"
-  } else if (feet > 2500) {
+  } else if (feet > 2500){
     return "No can do"
-  } else if (feet > 2000) {
+  } else if (feet > 2000){
     return "'I will gladly take your thirty bucks."
   }
 }
